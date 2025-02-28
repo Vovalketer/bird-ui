2bb3222103e82fbd0546f80ad6ce2465ec142a91
Vovalketer <kevinaa021@gmail.com>
2025-02-27 23:43:19 -0300

    feat: add interceptor to attach the JWT to the requests

--- lib/services/api/birdApi.ts
+++ lib/services/api/birdApi.ts
@@ -1,8 +1,30 @@
+"use client";
 import axios from "axios";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
 export const axiosClient = axios.create({
 	baseURL: "http://localhost:8080",
+	headers: { "Content-Type": "application/json" },
 });
+let lastSession: Session | null = null;
+axiosClient.interceptors.request.use(
+	async (request) => {
+		if (lastSession == null || Date.now() > Date.parse(lastSession.expires)) {
+			const session = await getSession();
+			lastSession = session;
+		}
+		if (lastSession) {
+			request.headers.Authorization = `Bearer ${lastSession.accessToken}`;
+		} else {
+			request.headers.Authorization = undefined;
+		}
+		return request;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
 
 export const fetcher = async <T>(url: string) => {
 	const res = await axiosClient.get<T>(url);
