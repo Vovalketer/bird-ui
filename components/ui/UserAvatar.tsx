3e86662e85d0a8600b6c86fbc92aecf962e710cb
Vovalketer <kevinaa021@gmail.com>
2025-03-10 14:54:35 -0300

    refactor: extract user avatar into its own component

---
+++ components/ui/UserAvatar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import ProfilePlaceholder from "@/public/profile-placeholder.png";
+
+interface UserAvatarProps {
+	profileImage?: string;
+}
+export default function UserAvatar({ profileImage }: UserAvatarProps) {
+	return (
+		<Image
+			className="avatar size-12 rounded-full"
+			src={profileImage ? profileImage : ProfilePlaceholder}
+			width={100}
+			height={100}
+			alt=""
+		/>
+	);
+}
--- components/ui/UserHeader.tsx
+++ components/ui/UserHeader.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
-import ProfilePlaceholder from "@/public/profile-placeholder.png";
+import UserAvatar from "./UserAvatar";
 
 interface UserHeaderProps {
 	username: string;
@@ -17,14 +16,7 @@ export default function UserHeader({
 	return (
 		<>
 			<Link href={url} className="flex">
-				<div className="size-12">
-					<Image
-						src={profileImage ? profileImage : ProfilePlaceholder}
-						width={400}
-						height={400}
-						alt=""
-					/>
-				</div>
+				<UserAvatar profileImage={profileImage} />
 				<div className="flex flex-col place-content-center pl-2 text-sm">
 					<p className="font-semibold">{handle}</p>
 					<p className="text-light-text-secondary dark:text-dark-text-secondary">
