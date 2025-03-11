0fdbe0558258b2e4a3f7b59f06ec31cea4ee9d10
Vovalketer <kevinaa021@gmail.com>
2025-03-11 17:09:35 -0300

    feat: add generic error handler for axios errors

---
+++ lib/services/utils/errorHandler.ts
@@ -0,0 +1,32 @@
+import { ErrorDetail } from "@/lib/types/domain/error";
+import { ErrorResponse } from "@/lib/types/external/error";
+import { AxiosError, isAxiosError } from "axios";
+
+export default function errorHandler(
+	error: unknown,
+	unknownErrorMessage?: string,
+) {
+	if (isAxiosError(error)) {
+		const err: AxiosError<ErrorResponse> = error;
+		if (err.response) {
+			const errResponse: ErrorDetail[] = err.response?.data.errors.map(
+				(error) => {
+					return {
+						cause: error.source ? error.source.parameter : error.title,
+						message: error.detail,
+					};
+				},
+			);
+
+			return {
+				success: false,
+				errors: errResponse,
+			};
+		}
+	}
+	if (unknownErrorMessage) {
+		throw new Error(unknownErrorMessage);
+	} else {
+		throw new Error("An unexpected error has occurred");
+	}
+}
