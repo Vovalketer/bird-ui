48218c0a78a1a6492e4634c9bca064dacadde389
Vovalketer <kevinaa021@gmail.com>
2025-03-11 15:15:32 -0300

    feat: add interceptors to the server http client

--- lib/services/api/birdApiServer.ts
+++ lib/services/api/birdApiServer.ts
@@ -1,5 +1,8 @@
 "use server";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 import axios from "axios";
+import { getServerSession, Session } from "next-auth";
+
 export const axiosBaseClient = axios.create({
 	baseURL: "http://localhost:8080",
 });
@@ -8,3 +11,22 @@ export const axiosClient = axios.create({
 	baseURL: "http://localhost:8080",
 	headers: { "Content-Type": "application/json" },
 });
+
+let lastSession: Session | null = null;
+axiosClient.interceptors.request.use(
+	async (request) => {
+		if (!lastSession || Date.now() > Date.parse(lastSession.expires)) {
+			const session = await getServerSession(options);
+			lastSession = session;
+		}
+		if (lastSession) {
+			request.headers.Authorization = `Bearer ${lastSession.accessToken}`;
+		} else {
+			request.headers.Authorization = undefined;
+		}
+		return request;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
