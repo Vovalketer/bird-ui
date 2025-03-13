f38fb1011a11b4e5434adff27ef4c52d09cfda70
Vovalketer <kevinaa021@gmail.com>
2025-03-13 09:37:09 -0300

    chore: remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,150 @@
-import Image from "next/image";
+import { PostResource } from "@/lib/types/external/postApi";
+import PostList from "../components/ui/PostList";
+import { UserResource } from "@/lib/types/external/userApi";
+
+const user1: UserResource = {
+	id: "cbf9f660-570d-4049-8d71-c0efab274f4e",
+	type: "users",
+	attributes: {
+		username: "testusername",
+		handle: "testhandle",
+		bio: "testbio",
+		dateOfBirth: new Date(),
+		location: "testlocation",
+		profileImage: "https://www.pngall.com/wp-content/uploads/5/Profile.png",
+		createdAt: new Date(),
+	},
+	links: {
+		self: "http://localhost:8080/users/1",
+	},
+};
+
+const user2: UserResource = {
+	id: "de3a8ccf-8b65-4e1a-841e-a4448bba0c4f",
+	type: "users",
+	attributes: {
+		username: "testusername2",
+		handle: "testhandle2",
+		bio: "testbio",
+		dateOfBirth: new Date(),
+		location: "testlocation",
+		profileImage: "https://www.pngall.com/wp-content/uploads/5/Profile.png",
+		createdAt: new Date(),
+	},
+	links: {
+		self: "http://localhost:8080/users/1",
+	},
+};
+const post1: PostResource = {
+	id: 2,
+	type: "posts",
+	attributes: {
+		text: "testtext",
+		replyType: "EVERYONE",
+		createdAt: new Date(),
+	},
+	relationships: {
+		user: {
+			data: { id: "cbf9f660-570d-4049-8d71-c0efab274f4e", type: "users" },
+			links: { self: "http://localhost:8080/users/testusername" },
+		},
+		parent: {
+			data: { id: 1, type: "posts" },
+			links: { self: "http://localhost:8080/posts/1" },
+		},
+		media: {
+			data: [{ id: 1, type: "media" }],
+			links: { self: "http://localhost:8080/media/1" },
+		},
+	},
+	metadata: {
+		interactions: {
+			repliesCount: 0,
+			repostsCount: 0,
+			likesCount: 0,
+			isLiked: false,
+			isReposted: false,
+		},
+	},
+	links: {
+		self: "http://localhost:8080/posts/2",
+		related: "http://localhost:8080/posts/1/replies",
+	},
+};
+const post2: PostResource = {
+	id: 3,
+	type: "posts",
+	attributes: {
+		text: "testtext2",
+		replyType: "EVERYONE",
+		createdAt: new Date(),
+	},
+	relationships: {
+		user: {
+			data: { id: "cbf9f660-570d-4049-8d71-c0efab274f4e", type: "users" },
+			links: { self: "http://localhost:8080/users/testusername" },
+		},
+		parent: {
+			data: { id: 1, type: "posts" },
+			links: { self: "http://localhost:8080/posts/1" },
+		},
+		media: {
+			data: [{ id: 1, type: "media" }],
+			links: { self: "http://localhost:8080/media/1" },
+		},
+	},
+	metadata: {
+		interactions: {
+			repliesCount: 0,
+			repostsCount: 0,
+			likesCount: 0,
+			isLiked: false,
+			isReposted: false,
+		},
+	},
+	links: {
+		self: "http://localhost:8080/posts/3",
+		related: "http://localhost:8080/posts/1/replies",
+	},
+};
+const post3: PostResource = {
+	id: 4,
+	type: "posts",
+	attributes: {
+		text: "testtext3",
+		replyType: "EVERYONE",
+		createdAt: new Date(),
+	},
+	relationships: {
+		user: {
+			data: { id: "de3a8ccf-8b65-4e1a-841e-a4448bba0c4f", type: "users" },
+			links: { self: "http://localhost:8080/users/testusername" },
+		},
+		media: {
+			data: [{ id: 1, type: "media" }],
+			links: { self: "http://localhost:8080/media/1" },
+		},
+	},
+	metadata: {
+		interactions: {
+			repliesCount: 0,
+			repostsCount: 0,
+			likesCount: 0,
+			isLiked: false,
+			isReposted: false,
+		},
+	},
+	links: {
+		self: "http://localhost:8080/posts/4",
+	},
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const posts = [post1, post2, post3];
+	const users = [user1, user2];
+	return (
+		<>
+			<p>hi</p>
+		</>
+	);
 }
