2b4ac311bab57ee04d3a33016e07ba5a5be442ac
Vovalketer <kevinaa021@gmail.com>
2025-03-13 09:10:00 -0300

    feat: add infinite scroll component

---
+++ components/InfiniteList.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+interface InfiniteListProps {
+	children: React.ReactNode;
+	isLoading: boolean;
+	onLoadMore: () => void;
+}
+export default function InfiniteList({
+	children,
+	onLoadMore,
+	isLoading,
+}: InfiniteListProps) {
+	const { ref, inView } = useInView({ threshold: 0, delay: 1000 });
+
+	useEffect(() => {
+		if (inView && !isLoading) {
+			onLoadMore();
+		}
+	}, [inView, isLoading, onLoadMore]);
+
+	return (
+		<>
+			<section>{children}</section>
+			<div className="observer" ref={ref}></div>;
+		</>
+	);
+}
