13c64da336338d991cfc60bf8c8d233a7d293ba2
Vovalketer <kevinaa021@gmail.com>
2025-03-15 19:34:27 -0300

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -13,9 +13,16 @@ const config: Config = {
 	clearMocks: true,
 	collectCoverage: true,
 	coverageDirectory: "coverage",
+	moduleNameMapper: {
+		"^jose": require.resolve("jose"),
+		"^@panva/hkdf": require.resolve("@panva/hkdf"),
+		"^preact-render-to-string": require.resolve("preact-render-to-string"),
+		"^preact": require.resolve("preact"),
+		"^uuid": require.resolve("uuid"),
+	},
 
 	// Add more setup options before each test is run
-	// setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
