44a175265b16e614050bff9f43c998e2bc536903
Vovalketer <kevinaa021@gmail.com>
2025-03-21 01:31:50 -0300

    refactor: extract PostComposer from the NewPostModal

--- components/ui/NewPostModal.tsx
+++ components/ui/NewPostModal.tsx
@@ -1,11 +1,7 @@
-import submitPost from "@/lib/actions/posts/submitPost";
 import Post from "@/lib/types/domain/post";
-import { CreatePostRequestSchema } from "@/lib/types/external/createPostRequest";
-import toast from "react-hot-toast";
-import FormButton from "./form/FormButton";
 import Modal from "./Modal";
 import PostCard from "./PostCard";
-import UserAvatar from "./UserAvatar";
+import PostComposer from "./PostComposer";
 
 interface NewPostModalProps {
 	isOpen: boolean;
@@ -19,64 +15,16 @@ export default function NewPostModal({
 	profileImage,
 	post,
 }: NewPostModalProps) {
-	const handlePost = async (formData: FormData) => {
-		//TODO: handle media
-		const form = {
-			text: formData.get("text"),
-			replyType: post ? "EVERYONE" : formData.get("replyType"),
-		};
-		const parsedForm = CreatePostRequestSchema.safeParse(form);
-		if (!parsedForm.success) {
-			toast.error(parsedForm.error.message);
-			return;
-		}
-
-		const res = await submitPost(parsedForm.data, post?.id);
-		if (res.success) {
-			toast.success(
-				post ? "Reply sent successfully" : "Post sent successfully",
-			);
-		} else {
-			res.errors?.forEach((error) => {
-				toast.error(`${error.cause}: ${error.message}`);
-			});
-			toast.error("Failed to send post");
-		}
-	};
 	return (
 		<Modal onClose={onClose} isOpen={isOpen}>
 			<section className="flex flex-col gap-y-4 m-2 ">
 				{post && <PostCard key={post.id} post={post} />}
-				<form action={handlePost} className="flex">
-					<div>
-						<UserAvatar profileImage={profileImage} />
-					</div>
-					<div className="flex flex-col w-full gap-y-4">
-						<div className="pl-2 pr-1">
-							<textarea
-								name="text"
-								placeholder={post ? "Write your reply" : "What's happening?"}
-								className="textarea textarea-ghost box-border resize-none w-full min-h-32"
-							/>
-						</div>
-						<div className="flex justify-between">
-							<select
-								name="replyType"
-								defaultValue={"EVERYONE"}
-								className="select"
-							>
-								<option value="EVERYONE">Everyone can reply</option>
-								<option value="FOLLOWERS">Followers can reply</option>
-								<option value="MENTIONS">Mentioned users can reply</option>
-							</select>
-							<div className="place-self-end">
-								<FormButton type="submit">
-									{!post ? "Post" : "Reply"}
-								</FormButton>
-							</div>
-						</div>
-					</div>
-				</form>
+				<PostComposer
+					profileImage={profileImage}
+					replyingToPostId={post?.id}
+					minRows={3}
+					maxRows={10}
+				/>
 			</section>
 		</Modal>
 	);
---
+++ components/ui/PostComposer.tsx
@@ -0,0 +1,84 @@
+import TextareaAutosize from "react-textarea-autosize";
+import PrimaryButton from "./base/PrimaryButton";
+import UserAvatar from "./UserAvatar";
+import submitPost from "@/lib/actions/posts/submitPost";
+import { CreatePostRequestSchema } from "@/lib/types/external/createPostRequest";
+import toast from "react-hot-toast";
+
+interface ReplyTextAreaProps {
+	profileImage?: string;
+	minRows?: number;
+	maxRows?: number;
+	replyingToPostId?: number;
+	borders?: boolean;
+}
+export default function PostComposer({
+	profileImage,
+	minRows,
+	maxRows,
+	replyingToPostId,
+	borders,
+}: ReplyTextAreaProps) {
+	const isReply = replyingToPostId !== undefined;
+	const borderClass = borders
+		? "border-2 border-light-border dark:border-dark-border"
+		: "";
+	const handlePost = async (formData: FormData) => {
+		//TODO: handle media
+		const form = {
+			text: formData.get("text"),
+			replyType: isReply ? "EVERYONE" : formData.get("replyType"),
+		};
+		const parsedForm = CreatePostRequestSchema.safeParse(form);
+		if (!parsedForm.success) {
+			toast.error(parsedForm.error.message);
+			return;
+		}
+
+		const res = await submitPost(parsedForm.data, replyingToPostId);
+		if (res.success) {
+			toast.success(
+				isReply ? "Reply sent successfully" : "Post sent successfully",
+			);
+		} else {
+			res.errors?.forEach((error) => {
+				toast.error(`${error.cause}: ${error.message}`);
+			});
+			toast.error("Failed to send post");
+		}
+	};
+	return (
+		<form action={handlePost} className={`flex p-2 gap-y-4 ${borderClass}`}>
+			<UserAvatar profileImage={profileImage} />
+			<div className="flex flex-col w-full gap-y-4 ml-2">
+				<div className="pl-2 pr-1">
+					<TextareaAutosize
+						name="text"
+						placeholder={isReply ? "Write your reply" : "What's happening?"}
+						className="textarea textarea-ghost box-border resize-none w-full min-h-2 mt-1"
+						minRows={minRows}
+						maxRows={maxRows}
+					/>
+				</div>
+				<div className="flex justify-between my-2">
+					{!isReply && (
+						<select
+							name="replyType"
+							defaultValue={"EVERYONE"}
+							className="select w-fit"
+						>
+							<option value="EVERYONE">Everyone can reply</option>
+							<option value="FOLLOWERS">Followers can reply</option>
+							<option value="MENTIONS">Mentioned users can reply</option>
+						</select>
+					)}
+					<div className="ml-auto">
+						<PrimaryButton type="submit">
+							{isReply ? "Reply" : "Post"}
+						</PrimaryButton>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
+}
