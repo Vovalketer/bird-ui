f5fe8f55a3fa25c0cd95df92b4be9806307cb555
Vovalketer <kevinaa021@gmail.com>
2025-03-21 01:49:30 -0300

    feat: add Divider component

---
+++ components/ui/Divider.tsx
@@ -0,0 +1,5 @@
+export default function Divider() {
+	return (
+		<div className="divider before:bg-light-border after:bg-light-border dark:before:bg-dark-border dark:after:bg-dark-border" />
+	);
+}
