351dc7bdb02391ddf61101222f9a7bb2c70ca68a
Vovalketer <kevinaa021@gmail.com>
2025-03-21 01:50:41 -0300

    refactor: add prop to set the NewPostButton in a fixed position at the bottom right

--- components/ui/NewPostButton.tsx
+++ components/ui/NewPostButton.tsx
@@ -2,9 +2,22 @@
 import { useNewPostModal } from "@/context/NewPostModalContext";
 import PrimaryButton from "./base/PrimaryButton";
 
-export default function NewPostButton() {
+interface NewPostButtonProps {
+	fixedPosition?: boolean;
+}
+export default function NewPostButton({ fixedPosition }: NewPostButtonProps) {
 	const { openModal } = useNewPostModal();
-	return (
+	return fixedPosition ? (
+		<div className="fixed bottom-4 right-4">
+			<PrimaryButton
+				onClick={() => {
+					openModal();
+				}}
+			>
+				+ Post
+			</PrimaryButton>
+		</div>
+	) : (
 		<PrimaryButton
 			onClick={() => {
 				openModal();
