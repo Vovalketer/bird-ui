725c2b4165755c62ea005b10e00f2a0d37cb169c
Vovalketer <kevinaa021@gmail.com>
2025-03-22 02:20:23 -0300

    refactor(NewPostModal): remove profileImage props since the PostComposer will grab it on its own

--- components/ui/NewPostModal.tsx
+++ components/ui/NewPostModal.tsx
@@ -6,25 +6,18 @@ import PostComposer from "./PostComposer";
 interface NewPostModalProps {
 	isOpen: boolean;
 	onClose: () => void;
-	profileImage?: string;
 	post?: Post;
 }
 export default function NewPostModal({
 	isOpen,
 	onClose,
-	profileImage,
 	post,
 }: NewPostModalProps) {
 	return (
 		<Modal onClose={onClose} isOpen={isOpen}>
 			<section className="flex flex-col gap-y-4 m-2 ">
 				{post && <PostCard key={post.id} post={post} />}
-				<PostComposer
-					profileImage={profileImage}
-					replyingToPostId={post?.id}
-					minRows={3}
-					maxRows={10}
-				/>
+				<PostComposer replyingToPostId={post?.id} minRows={3} maxRows={10} />
 			</section>
 		</Modal>
 	);
