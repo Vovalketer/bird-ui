6c99d309c068e7c49221dce9c87dcdc1d90b0888
Vovalketer <kevinaa021@gmail.com>
2025-03-23 02:35:43 -0300

    refactor: add "tab" parameter to the user data fetch hook

--- lib/services/api/hooks/useUserInfinitePosts.ts
+++ lib/services/api/hooks/useUserInfinitePosts.ts
@@ -1,5 +1,5 @@
 import { useInfinitePosts } from "./useInfinitePosts";
 
-export function useUserInfinitePosts(username: string) {
-	return useInfinitePosts(`/api/users/${username}/posts`);
+export function useUserInfinitePosts(username: string, tab?: string) {
+	return useInfinitePosts(`/api/users/${username}/${tab}`);
 }
