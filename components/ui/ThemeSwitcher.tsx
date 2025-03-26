c0b23d5c3ddd18f942055766651c36fdc0580850
Vovalketer <kevinaa021@gmail.com>
2025-03-25 22:21:09 -0300

    refactor: use generic button on the ThemeSwitcher

--- components/ui/ThemeSwitcher.tsx
+++ components/ui/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import SecondaryElementStyle from "./SecondaryElement";
+import Button from "./Button";
 
 export default function ThemeSwitcher() {
 	const [mounted, setMounted] = useState(false);
@@ -13,23 +13,13 @@ export default function ThemeSwitcher() {
 	//TODO: return placeholder instead of null
 	return mounted ? (
 		resolvedTheme === "dark" ? (
-			<SecondaryElementStyle>
-				<button
-					className="px-4 py-2 -mx-1 -my-0.5"
-					onClick={() => setTheme("light")}
-				>
-					<SunIcon role="img" width={24} height={24} />
-				</button>
-			</SecondaryElementStyle>
+			<Button color="secondary" onClick={() => setTheme("light")}>
+				<SunIcon role="img" width={24} height={24} />
+			</Button>
 		) : (
-			<SecondaryElementStyle>
-				<button
-					className="px-4 py-2 -mx-1 -my-0.5"
-					onClick={() => setTheme("dark")}
-				>
-					<MoonIcon role="img" width={24} height={24} />
-				</button>
-			</SecondaryElementStyle>
+			<Button color="secondary" onClick={() => setTheme("dark")}>
+				<MoonIcon role="img" width={24} height={24} />
+			</Button>
 		)
 	) : null;
 }
